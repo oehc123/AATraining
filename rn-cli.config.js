07d62dca6cdccf5a193711754a7be8f8129ae83e
oehc123 <josesvz@gmail.com>
2020-06-03 13:56:11 -0400

    restructoring files and adding typescript

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -3,6 +3,12 @@ const blacklist = require('metro-config/src/defaults/blacklist')
 module.exports = {
   resolver: {
     blacklistRE: blacklist([/\/youi\/build\/.*/])
+  },
+  getTransformModulePath() {
+    return require.resolve('react-native-typescript-transformer');
+  },
+  getSourceExts() {
+    return ['ts', 'tsx'];
   }
 };
 
